4e63cee0575afe34a19a4748593edb9cac2f1025
lkhaolh <lkhaolh@gmail.com>
2022-07-18 14:46:01 +0700

    first commit

---
+++ process.php
@@ -0,0 +1,14 @@
+<?php 
+
+echo "Bạn tên là "  .$_POST['ten'] ."<br>";
+
+echo "Giới tính của bạn là "  .$_POST['gioi_tinh'] ."<br>";
+
+echo "Email của bạn là "  .$_POST['email'] ."<br>";
+
+echo "Mật khẩu "  .$_POST['mat_khau'] ."<br>";
+
+echo "Sở thích "  .$_POST['so_thich'] ."<br>";
+
+
+
